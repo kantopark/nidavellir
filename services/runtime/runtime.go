a401430ae9f54696929402993bf2566ad6f8b677
Daniel Bok <daniel.bok@outlook.com>
2020-01-05 22:18:17 +0800

    Moved runtime reader as service

--- services/runtime/runtime.go
+++ services/runtime/runtime.go
@@ -1,7 +1,9 @@
-package libs
+package runtime
 
 import (
 	"io/ioutil"
+	"path/filepath"
+	"strings"
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
@@ -24,7 +26,28 @@ type Task struct {
 	Env  map[string]string `yaml:"env"`
 }
 
-func ReadRuntime(path string) (*Runtime, error) {
+func FromDir(dir string) (*Runtime, error) {
+	info, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not read working directory files")
+	}
+
+	file := ""
+	for _, f := range info {
+		fn := strings.ToLower(f.Name())
+		if !f.IsDir() && fn == "runtime.yaml" || fn == "runtime.yml" {
+			file = filepath.Join(dir, f.Name())
+		}
+	}
+
+	if file == "" {
+		return nil, errors.New("no runtime.yaml found in working directory")
+	}
+
+	return New(file)
+}
+
+func New(path string) (*Runtime, error) {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not read file content")
