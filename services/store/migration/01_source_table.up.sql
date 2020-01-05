e288dd1478aa0add8ad8eef41598e3cc8ad7677f
Daniel Bok <daniel.bok@outlook.com>
2020-01-05 12:34:01 +0800

    Combine schedule and source table
    Add min length check in postgres table for name

--- services/store/migration/01_source_table.up.sql
+++ services/store/migration/01_source_table.up.sql
@@ -1,7 +1,13 @@
 CREATE TABLE IF NOT EXISTS source
 (
     id         SERIAL PRIMARY KEY,
-    name       VARCHAR(100) UNIQUE,
-    repo_url   VARCHAR,
-    commit_tag VARCHAR(40)
+    name       VARCHAR(100) CHECK ( length(name) >= 4 ) UNIQUE,
+    repo_url   VARCHAR(2000),
+    commit_tag VARCHAR(40),
+    interval   INTEGER     NOT NULL,
+    state      VARCHAR(20) NOT NULL,
+    next_time  TIMESTAMP   NOT NULL
 );
+
+CREATE INDEX source_state_next_time ON source
+    (state, next_time);
--- services/store/migration/03_schedule_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS schedule_state_next_time;
-DROP TABLE IF EXISTS schedule;
